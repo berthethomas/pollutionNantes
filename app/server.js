33b12d35259a39fad223146f1726f05be6e3de2f
Thomas Berthé <berthe.thomas01@gmail.com>
2017-10-05 16:31:48 +0200

    indent update

--- app/server.js
+++ app/server.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
-
 var express = require('express'),
-	cors = require('cors'),
-	path = require('path');
+        cors = require('cors'),
+        path = require('path');
 
 //controllers
 var pollution = require('./controllers/pollution');
 
 //application
-var	app = express();
+var app = express();
 
 app.use(cors());
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodiess
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodiess
 
 //routes
 app.use('/pollution', pollution);
